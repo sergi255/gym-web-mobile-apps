677453ebfabe44937bef1ac903a655716beab420
PawelSobiech <pawelsobiech221@gmail.com>
2023-12-13 18:12:29 +0100

    commit

--- mobile/screens/Home.tsx
+++ mobile/screens/Home.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 import { styles } from '../styles/styles';
 
 const Welcome = () => {
   return (
     <View style={styles.container}>
+        <img src="../assets/logo.png" alt="logo"/>
         <Text style={styles.header}>Dyszak</Text>
         <Text style={styles.minorHeader}>Cardio Camp</Text>
     </View>
